f5802dceaf9112528ce87882e7bced58cea3c463
Reid Olson <reid.olson@gmail.com>
2026-09-10 16:06:30 -0700

    there we go

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 //Write your code here
 
 
+const attendee = {
+  attendeeId: "T001",
+  name: "alice smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150
+};
+
+function logAttendeeName() {
+  console.log(attendee.name);
+}
+
+function logTicketPrice() {
+  console.log(attendee.ticketPrice);
+}
+
+function updateTicketType(newType) {
+  attendee.ticketType = newType;
+}
+
+function removeEventProperty() {
+  delete attendee.event;
+}
+
+function addCheckedInProperty() {
+  attendee.checkedIn = true;
+}
+
+
 
 //Needed for the tests to work. Don't modify
 module.exports = {
